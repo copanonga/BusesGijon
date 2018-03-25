d5b4886cf99594fe2ab740892b1a4257d0c8e315
Jose <josemsg@gmail.com>
2018-03-25 11:23:34 +0200

    Actualizada petición de datos a AFNetworking 3.x

--- Buses Gijón/Peticiones/Peticiones.m
+++ Buses Gijón/Peticiones/Peticiones.m
@@ -24,84 +24,84 @@
 
 -(void)getInfo {
 	
-	NSString *URL = URL_INFO;
-	
-	AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-	manager.requestSerializer=[AFHTTPRequestSerializer serializer];
-	manager.responseSerializer= [AFHTTPResponseSerializer serializer];
-	[manager GET:URL parameters:nil success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
-		
-		NSDictionary* json = [NSJSONSerialization
-							  JSONObjectWithData:responseObject
-							  options:kNilOptions
-							  error:nil];
-		
-		NSMutableArray *listadoLineas = [[NSMutableArray alloc] init];
-		
-		NSDictionary *lineasTotal = [json objectForKey:@"lineas"];
-		NSArray *lineas = [lineasTotal objectForKey:@"linea"];
-		[lineas enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-			
-			Linea *lineaObtenida = [[Linea alloc]init];
-			[lineaObtenida setLinea:obj];
-			[listadoLineas addObject:lineaObtenida];
-			
-		}];
-		
-		NSMutableArray *listadoParadas = [[NSMutableArray alloc] init];
-		
-		NSDictionary *paradasTotal = [json objectForKey:@"paradas"];
-		NSArray *paradas = [paradasTotal objectForKey:@"parada"];
-		[paradas enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-			
-			Parada *paradaObtenida = [[Parada alloc]init];
-			[paradaObtenida setParada:obj];
-			[listadoParadas addObject:paradaObtenida];
-			
-		}];
-		
-		NSMutableArray *listadoParadasTrayectos = [[NSMutableArray alloc] init];
-		
-		NSDictionary *paradasTrayectosTotal = [json objectForKey:@"paradasTrayectos"];
-		NSArray *paradasTrayectos = [paradasTrayectosTotal objectForKey:@"parada"];
-		[paradasTrayectos enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-			
-			Parada *paradaObtenida = [[Parada alloc]init];
-			[paradaObtenida setParada:obj];
-			[listadoParadasTrayectos addObject:paradaObtenida];
-			
-		}];
-		
-		NSMutableArray *listadoHorarios = [[NSMutableArray alloc] init];
-		
-		NSDictionary *horariosTotal = [json objectForKey:@"horarios"];
-		NSArray *horarios = [horariosTotal objectForKey:@"horario"];
-		[horarios enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-			
-			Horario *horarioObtenido = [[Horario alloc]init];
-			[horarioObtenido setHorario:obj];
-			[listadoHorarios addObject:horarioObtenido];
-			
-		}];
-		
-		NSMutableArray *listadoTrayectos = [[NSMutableArray alloc] init];
-		
-		NSDictionary *trayectosTotal = [json objectForKey:@"trayectos"];
-		NSArray *trayectos = [trayectosTotal objectForKey:@"trayecto"];
-		[trayectos enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-			
-			Trayecto *trayectoObtenido = [[Trayecto alloc]init];
-			[trayectoObtenido setTrayecto:obj];
-			[listadoTrayectos addObject:trayectoObtenido];
-			
-		}];
-		
-		[self.delegate getInfoBuses:listadoParadas :listadoLineas :listadoTrayectos :listadoHorarios :listadoParadasTrayectos];
-		
-	} failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
-		[self.delegate showError:@"Ayuntamiento de Gijón: datos no disponibles temporalmente"];
-	}];
-	
+    NSURL *URL = [NSURL URLWithString:URL_INFO];
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.requestSerializer=[AFHTTPRequestSerializer serializer];
+    manager.responseSerializer= [AFHTTPResponseSerializer serializer];
+    [manager GET:URL.absoluteString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+        
+        NSDictionary* json = [NSJSONSerialization
+                              JSONObjectWithData:responseObject
+                              options:kNilOptions
+                              error:nil];
+        
+        NSMutableArray *listadoLineas = [[NSMutableArray alloc] init];
+        
+        NSDictionary *lineasTotal = [json objectForKey:@"lineas"];
+        NSArray *lineas = [lineasTotal objectForKey:@"linea"];
+        [lineas enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            Linea *lineaObtenida = [[Linea alloc]init];
+            [lineaObtenida setLinea:obj];
+            [listadoLineas addObject:lineaObtenida];
+            
+        }];
+        
+        NSMutableArray *listadoParadas = [[NSMutableArray alloc] init];
+        
+        NSDictionary *paradasTotal = [json objectForKey:@"paradas"];
+        NSArray *paradas = [paradasTotal objectForKey:@"parada"];
+        [paradas enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            Parada *paradaObtenida = [[Parada alloc]init];
+            [paradaObtenida setParada:obj];
+            [listadoParadas addObject:paradaObtenida];
+            
+        }];
+        
+        NSMutableArray *listadoParadasTrayectos = [[NSMutableArray alloc] init];
+        
+        NSDictionary *paradasTrayectosTotal = [json objectForKey:@"paradasTrayectos"];
+        NSArray *paradasTrayectos = [paradasTrayectosTotal objectForKey:@"parada"];
+        [paradasTrayectos enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            Parada *paradaObtenida = [[Parada alloc]init];
+            [paradaObtenida setParada:obj];
+            [listadoParadasTrayectos addObject:paradaObtenida];
+            
+        }];
+        
+        NSMutableArray *listadoHorarios = [[NSMutableArray alloc] init];
+        
+        NSDictionary *horariosTotal = [json objectForKey:@"horarios"];
+        NSArray *horarios = [horariosTotal objectForKey:@"horario"];
+        [horarios enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            Horario *horarioObtenido = [[Horario alloc]init];
+            [horarioObtenido setHorario:obj];
+            [listadoHorarios addObject:horarioObtenido];
+            
+        }];
+        
+        NSMutableArray *listadoTrayectos = [[NSMutableArray alloc] init];
+        
+        NSDictionary *trayectosTotal = [json objectForKey:@"trayectos"];
+        NSArray *trayectos = [trayectosTotal objectForKey:@"trayecto"];
+        [trayectos enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            Trayecto *trayectoObtenido = [[Trayecto alloc]init];
+            [trayectoObtenido setTrayecto:obj];
+            [listadoTrayectos addObject:trayectoObtenido];
+            
+        }];
+        
+        [self.delegate getInfoBuses:listadoParadas :listadoLineas :listadoTrayectos :listadoHorarios :listadoParadasTrayectos];
+        
+    } failure:^(NSURLSessionTask *operation, NSError *error) {
+        NSLog(@"Error getInfo: %@", error);
+        [self.delegate showError:@"Ayuntamiento de Gijón: datos no disponibles temporalmente"];
+    }];
+    
 }
 
 @end
